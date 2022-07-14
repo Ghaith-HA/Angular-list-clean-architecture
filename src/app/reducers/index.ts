10088583efb9f8908706a9f18a390e3dfc3d4fc2
Ghaith Hallawe <ghaith.h@storexweb.com>
2022-07-14 21:09:17 +0300

    add reducers to manage the state of search text and movie list data

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,20 +1,57 @@
 import { Action, createReducer, on } from '@ngrx/store';
-import { increment, decrement, reset } from '../actions/index';
+import { increment, decrement, reset, changeCounter, changeMovieList, changeSearchText } from '../actions/index';
+import { MovieList } from '../core/entity';
 
-export const initialState = 0;
+export const initialCounterState: number = 0;
+export const initialSearchTextState: string = '';
+export const initialMovieListState: MovieList[] = [];
 
-const _counterReducer = createReducer(initialState,
+const _counterReducer = createReducer(initialCounterState,
   on(increment, state => state + 1),
   on(decrement, state => state - 1),
   on(reset, state => 0),
+  on(changeCounter, (state, response) => {
+    if(response && response.counter){
+      return response.counter;
+    }
+    else{
+      return state
+    }
+    
+  }),
 );
 
-export function counterReducer(state: number, action: Action) {
+const _movieListReducer = createReducer(initialMovieListState,
+  on(changeMovieList, (state, response) => {
+    if(response && response.movieList && response.movieList.length){
+      return response.movieList;
+    }
+    else{
+      return state
+    }
+    
+  }),
+);
+
+const _searchTextReducer = createReducer(initialSearchTextState,
+    on(changeSearchText, (state, response) => {
+      if(response && response.searchText ){
+        return response.searchText;
+      }
+      else{
+        return state
+      }
+      
+    }),
+  )
+export function counterReducer(state: number, action: Action): any {
   return _counterReducer(state, action);
 }
 
+export function movieListReducer(state: MovieList[], action: Action): any {
+  return _movieListReducer(state, action);
+}
 
-/*
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://github.com/ngrx/platform
-*/
+export function searchTextReducer(state: string, action: Action): any {
+  return _searchTextReducer(state, action);
+}
