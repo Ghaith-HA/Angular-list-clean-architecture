56ca7b40bbc15116ac482811bcbe55bf22b420c9
Ghaith Hallawe <ghaith.h@storexweb.com>
2022-07-14 21:06:29 +0300

    add the repository services and provide them to data module

--- src/app/data/data.module.ts
+++ src/app/data/data.module.ts
@@ -1,8 +1,9 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {TodoRepository, UserListRepository} from '../core/repository';
+import {MovieListRepository, TodoRepository, UserListRepository} from '../core/repository';
 import {TodoRepositoryService} from './repository/todo-repository.service';
 import { UserListRepositoryService } from './repository/user-list.repository.service';
+import { MovieListRepositoryService } from './repository/movie-list-repository.service';
 
 @NgModule({
   declarations: [],
@@ -12,6 +13,7 @@ import { UserListRepositoryService } from './repository/user-list.repository.ser
   providers: [
     {provide: TodoRepository, useClass: TodoRepositoryService},
     {provide: UserListRepository, useClass: UserListRepositoryService},
+    {provide: MovieListRepository, useClass: MovieListRepositoryService},
   ]
 })
 export class DataModule {
