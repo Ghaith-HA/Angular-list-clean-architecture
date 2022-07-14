ab11c5568a48be6d3f48a6c58b2ce1c858070dbd
Ghaith Hallawe <ghaith.h@storexweb.com>
2022-07-14 21:04:01 +0300

    add movie config to fetch the data from moviedv

---
+++ src/app/config/movie.ts
@@ -0,0 +1,21 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const movieConfig = {
+    API: 'd26199050edb71775735031ecf32f1c8',
+    DICOVER: 'https://api.themoviedb.org/3/discover/movie?api_key=d26199050edb71775735031ecf32f1c8',
+    SEARCH: 'https://api.themoviedb.org/3/search/movie?api_key=d26199050edb71775735031ecf32f1c8',
+    DETAILS: 'https://api.themoviedb.org/3/movie',
+    IMAGE_SERVER_PATH: 'https://image.tmdb.org/t/p/original',
+  };
+  
+  /*
+   * For easier debugging in development mode, you can import the following file
+   * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+   *
+   * This import should be commented out in production mode because it will have a negative impact
+   * on performance if an error is thrown.
+   */
+  // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+  
