4cabd85ca5d3106abc2ab589793841185a1354af
Ghaith Hallawe <ghaith.h@storexweb.com>
2022-07-14 21:02:40 +0300

    add actions for managing the state

--- src/app/actions/index.ts
+++ src/app/actions/index.ts
@@ -1,5 +1,8 @@
 import { createAction } from '@ngrx/store';
 
-export const increment = createAction('[Counter Component] Increment');
-export const decrement = createAction('[Counter Component] Decrement');
-export const reset = createAction('[Counter Component] Reset');
+export const increment = createAction('[Counter] Increment');
+export const decrement = createAction('[Counter] Decrement');
+export const reset = createAction('[Counter] Reset');
+export const changeCounter = createAction('[Counter] ChangeCounter', (response: any) => response);
+export const changeMovieList = createAction('[Movies List] ChangeMovieList', (response: any) => response);
+export const changeSearchText = createAction('[Advanced Search ] ChangeSearchText', (response: any) => response);
